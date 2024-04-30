d3aaff624641562c8d72ed2f1f922c0dd2af35d8
Tamira Leber <tamira.leber@students.fhnw.ch>
2024-04-30 15:33:51 +0200

    disconnect database

--- src/main/java/ch/fhnw/webec/exercise/model/Category.java
+++ src/main/java/ch/fhnw/webec/exercise/model/Category.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotEmpty;
 public class Category implements SelectOption {
     @Id
     private int id;
-    @NotEmpty
+
     private String name;
 
     public int getId() {
