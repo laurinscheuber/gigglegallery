cd86b8ed6667b9abfaea872a2594c1f5376c77be
Tamira Leber <tamira.leber@students.fhnw.ch>
2024-05-26 15:07:27 +0200

    add GiphyServiceTests

--- src/test/java/ch/fhnw/webec/exercise/GiphyTests.java
+++
@@ -1,5 +0,0 @@
-package ch.fhnw.webec.exercise;
-
-public class GiphyTests {
-
-}
---
+++ src/test/java/service/GiphyServiceTests.java
@@ -0,0 +1,20 @@
+package service;
+
+import ch.fhnw.webec.exercise.service.GiphyService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.Map;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class GiphyServiceTests {
+    private GiphyService giphyService;
+
+    @Test
+    public void testSearchGiphy() throws Exception {
+
+
+    }
+
+
+}
