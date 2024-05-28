886a7a7b9f4e422660c7dfe4cdb38e1b5d267236
laurinscheuber <38795964+laurinscheuber@users.noreply.github.com>
2024-05-28 04:59:59 +0200

    fix pipeline

--- src/main/java/ch/fhnw/webec/exercise/repository/CategoryRepository.java
+++ src/main/java/ch/fhnw/webec/exercise/repository/CategoryRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, String> {
-    List<Category> findCategoryBy(Long userId);
+ //   List<Category> findCategoryBy(Long id);
 }
