20123ca534a8b539b9120aaa6034468e89e3209b
laurinscheuber <38795964+laurinscheuber@users.noreply.github.com>
2024-05-28 10:43:59 +0200

    add category overvie in category.peb and category-add.peb

--- src/main/java/ch/fhnw/webec/exercise/controller/CategoryController.java
+++ src/main/java/ch/fhnw/webec/exercise/controller/CategoryController.java
@@ -1,22 +1,52 @@
 package ch.fhnw.webec.exercise.controller;
 
+import ch.fhnw.webec.exercise.model.Category;
 import ch.fhnw.webec.exercise.repository.CategoryRepository;
-import ch.fhnw.webec.exercise.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/category")
 public class CategoryController {
 
-    @Autowired
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
 
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
-    @RequestMapping(path = "/category", method = RequestMethod.GET)
-    public String category() {
+    @GetMapping
+    public String getAllCategories(Model model) {
+        model.addAttribute("categories", categoryRepository.findAll());
         return "category";
     }
 
+    @GetMapping("/add")
+    public String getAddCategoryForm() {
+        return "category-add";
+    }
+
+    @PostMapping("/add")
+    public String addCategory(@RequestParam String name) {
+        Category category = new Category();
+        category.setName(name);
+        categoryRepository.save(category);
+        return "redirect:/category";
+    }
+
+    @GetMapping("/{id}")
+    public String getEditCategoryForm(@PathVariable String id, Model model) {
+        Category category = categoryRepository.findById(id).orElseThrow();
+        model.addAttribute("category", category);
+        return "category-edit";
+    }
+
+    @PostMapping("/{id}")
+    public String editCategory(@PathVariable String id, @RequestParam String name) {
+        Category category = categoryRepository.findById(id).orElseThrow();
+        category.setName(name);
+        categoryRepository.save(category);
+        return "redirect:/category";
+    }
 }
