02ffad9e541acabb7de36a9f7e99849f0e369c34
laurinscheuber <38795964+laurinscheuber@users.noreply.github.com>
2024-05-28 17:32:45 +0200

    update package path

--- src/main/java/ch/fhnw/webec/exercise/config/GiphyConfig.java
+++ src/main/java/ch/fhnw/webec/exercise/config/GiphyConfig.java
@@ -1,4 +1,4 @@
-package com.example.giphy.config;
+package ch.fhnw.webec.exercise.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
