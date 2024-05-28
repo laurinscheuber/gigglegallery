5f94ad7779dbe3a027b921bf273ff4c516c9ed00
Tamira Leber <tamira.leber@students.fhnw.ch>
2024-05-28 02:27:55 +0200

    add @GetMapping to getAllUsers in UserService.java

--- src/main/java/ch/fhnw/webec/exercise/service/UserService.java
+++ src/main/java/ch/fhnw/webec/exercise/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Set;
@@ -56,7 +57,7 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);
     }
 
-
+    @GetMapping("/api/users")
     public List<Users> getAllUsers() {
         return userRepository.findAll();
     }
